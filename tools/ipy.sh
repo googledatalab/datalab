9597b5f54ceab1942b6a54e738852d101025429e
Nikhil Kothari <nikhilko@google.com>
2014-08-05 08:35:21 -0700

    Added a helper script to launch ipython
    The launched ipython includes our sources on the PYTHONPATH, as well as sets up the notebook dir to a folder within
    individual dev folders (/dev/$USER/notebooks).
    
    Change-Id: I5303ac68faf631a630018faf15b4096c8963660e

---
+++ tools/ipy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Script to launch IPython customized to current user in context of repository.
+
+USERNAME=$1
+if [ "$USERNAME" == "" ]; then
+  USERNAME=$USER
+fi
+
+USER_DIR=$REPO_DIR/dev/$USERNAME
+USER_PY_DIR=$USER_DIR/python
+USER_NB_DIR=$USER_DIR/notebooks
+
+# Create a notebooks directory
+mkdir -p $USER_NB_DIR
+
+# Setup python path to include modules being developed as well as any
+# developer specific modules.
+LIBS=$REPO_DIR/sources/sdk/pygcp
+export PYTHONPATH=$PYTHONPATH:$LIBS:$USER_PY_DIR
+
+# Spew out some informative output.
+echo 'Notebooks:' $USER_NB_DIR
+echo 'Lib Path :' $PYTHONPATH
+echo 'Local URL: http://localhost:9000'
+echo '----------'
+
+# Finally start IPython.
+ipython notebook --notebook-dir=$USER_NB_DIR \
+  --ip="*" --port=9000 \
+  --pylab=inline --no-mathjax --no-browser --no-script --quiet
+
