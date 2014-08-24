9e2b3abaedfe8b5b4bfc0ebc6f92481ffed189fc
Nikhil Kothari <nikhilko@google.com>
2014-08-23 22:50:33 -0700

    Demo VM dockerfile + build scripts to build python libs, stage & build on GCE
    Change-Id: Id06cc8dbb2c7947fc8a685e2fd35e83cd89d7fbf

---
+++ sources/ipython/build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+BUILD_DIR=$REPO_DIR/build/ipython
+mkdir -p $BUILD_DIR
+
+cp config.py $BUILD_DIR/config.py
+cp -R static $BUILD_DIR
+
+PYLIB_DIR=$REPO_DIR/build/python
+mkdir -p $PYLIB_DIR
+
+python setup.py sdist --dist-dir=$PYLIB_DIR
+mv MANIFEST $PYLIB_DIR/IPythonGCP.manifest
+
+cd $REPO_DIR/build
+tar cvfz ipython.tar.gz ipython
