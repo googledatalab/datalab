4967271d8b7d2523b5acbec5848a85cb6ee17222
Katya Rogers <katyarogers@google.com>
2014-08-25 11:25:23 -0700

    Added license.
    Change-Id: I43cdc0d4cba800770dde23a86d3192d952a3a4ba

--- tools/build.sh
+++ tools/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Build script for datastudio container running locally.
+# Build script for gcp/ipython container running locally.
 
-# To start a container running datastudio on localhost:8080, after build is done:
-#   docker run -p 8080:8080 --name datastudio datastudio:latest
+# To start a container running gcp/ipython on localhost:8080, after build is done:
+#   docker run -p 8080:8080 --name gcp gcp/ipython:latest
 # To stop running container:
-#   docker stop datastudio
+#   docker stop gcp
 # Running docker on Goobuntu: 
 #   http://go/installdocker
 
-docker build -t ipython ./docker/ipython
-docker build -t datastudio ..
+docker build -t google/gcp-ipython ./docker/ipython
+docker build -t google/gcp ..
 
