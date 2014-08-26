6e6ca31fe4b42c522b874904f570cb20f454316d
Drew Bryant <bryantd@google.com>
2014-08-26 08:59:37 -0700

    Added installation steps for gradle + node + typescript
    Change-Id: Ieda0d4521a85eb4d3c73708f1903e3c68435aa3f

--- tools/devinit.sh
+++ tools/devinit.sh
@@ -5,3 +5,19 @@
 # Python Linter (http://www.pylint.org)
 pip install pylint
 
+os=`uname`
+if [ "$os" == "Linux" ]; then
+  # Goobuntu package versions are all older than we need
+  # TODO(bryantd): See if possible to point aptitude at a fresher (trusted) pkg repo
+else
+  echo "Please install the following tools for your OS:"
+  echo "NodeJS 0.10.x: http://nodejs.org/download"
+  echo "Gradle 2.0: http://www.gradle.org/downloads"
+fi
+# TODO(bryantd): automate a cross-platform way to install NodeJS, Gradle
+
+if [ `which npm` != "" ]; then
+  npm install -g typescript
+else
+  echo "Please install NodeJS and then re-run this script to install the TypeScript compiler."
+fi
