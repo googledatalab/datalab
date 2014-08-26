fc6b91ff0db2f088030a1b65915fe57aff8463d4
Katya Rogers <katyarogers@google.com>
2014-08-25 20:55:02 -0700

    Container for preview release.
    Change-Id: I1ea245fe447c09e2390f30ff8e4024829e307187

---
+++ deploy/preview/preview_vm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Creates a new demo GCE VM instance. Useful logs on the VM:
+#   /var/log/startupscript.log
+#   /var/log/kubelet.log
+#   /var/log/docker.log
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: preview_vm.sh <instance_name>"
+    exit
+fi
+
+gcloud compute instances create $1 \
+  --image container-vm-v20140731 \
+  --image-project google-containers \
+  --zone us-central1-a \
+  --machine-type n1-standard-1 \
+  --maintenance-policy="TERMINATE" \
+  --tags http-server \
+  --scopes storage-full bigquery datastore sql \
+  --metadata-from-file google-container-manifest=preview_vm.yml
