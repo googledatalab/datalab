5b2e5b84976eeb67b9fb46d35afc86d1f5f3a331
Nikhil Kothari <nikhilko@google.com>
2014-10-01 14:03:10 -0700

    Docker container work
    - Add some additional python packages
    - Add gcloud sdk + support for external configuration
    - Add metadata tool for use in locally running containers

--- deploy/ipython/app.sh
+++ deploy/ipython/app.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# If this docker container instance is running outside of the cloud, then
+# start the local metadata service that works against gcloud, and export the
+# endpoint it is listening on for all child processes.
+if [ "$GAE_VM" = "" ]; then
+  export METADATA_HOST=localhost
+  export METADATA_PORT=8000
+
+  node /tools/metadata/server.js &
+fi
+
 ipython notebook --no-browser --no-mathjax --matplotlib inline \
   --ip="127.0.0.1" --port 8081 \
   --config=/ipython/config.py > /dev/null 2> /dev/null &
