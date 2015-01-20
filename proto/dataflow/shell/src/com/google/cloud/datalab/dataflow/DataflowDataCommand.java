2262d407bd771a66241574a4e52f839619d1b418
Nikhil Kothari <nikhilko@google.com>
2015-01-19 21:58:29 -0800

    Update to match new command api in ijava

--- proto/dataflow/shell/src/com/google/cloud/datalab/dataflow/DataflowDataCommand.java
+++ proto/dataflow/shell/src/com/google/cloud/datalab/dataflow/DataflowDataCommand.java
@@ -4,26 +4,29 @@
 package com.google.cloud.datalab.dataflow;
 
 import java.util.*;
+import com.beust.jcommander.*;
 import com.google.cloud.datalab.charting.data.*;
 import ijava.data.*;
 import ijava.extensibility.*;
 
-public final class DataflowDataCommand implements Command {
+public final class DataflowDataCommand extends Command<DataflowDataCommand.Options> {
 
   private final DataflowExtension _extension;
 
-  public DataflowDataCommand(DataflowExtension extension) {
+  public DataflowDataCommand(Shell shell, DataflowExtension extension) {
+    super(shell, Options.class);
     _extension = extension;
   }
 
   @Override
-  public Object evaluate(String arguments, String data, long evaluationID,
+  public Object evaluate(Options options, long evaluationID,
                          Map<String, Object> metadata) throws Exception {
     List<?> list = null;
+    String transform = options.transforms.get(0);
 
     InteractivePipelineResult pipelineResult = _extension.getPipelineResult();
     if (pipelineResult != null) {
-      list = pipelineResult.getCollection(arguments);
+      list = pipelineResult.getCollection(transform);
     }
 
     if (list != null) {
@@ -32,5 +35,12 @@ public final class DataflowDataCommand implements Command {
     }
 
     return new Data(null);
+  }
+
+
+  public static final class Options extends CommandOptions {
+
+    @Parameter(description  = "The name of the transform to whose data should be retrieved")
+    public List<String> transforms = new ArrayList();
   }
 }
