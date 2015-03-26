31e445382f5f5ff27d9b55852436eadcab1a8a70
Drew Bryant <bryantd@google.com>
2015-03-26 10:54:55 -0700

    Replace use of pushd/popd with absolute paths

--- sources/server/test.sh
+++ sources/server/test.sh
@@ -11,22 +11,23 @@ mkdir -p "$test_path";
 # Best solution likely involves generating the *.d.ts typedefs when building /src/* and correctly
 # symlinking these built files to the test directory, before building the tests.
 echo 'Testing DataLab server backend...';
+
 # Copy node .ts files to test.
 cp -r "$server_root/src/node" "$test_path";
 # Copy shared .ts files to the test area.
 cp -r "$server_root/src/shared" "$test_path/node/app";
 # Copy the test .ts files to the test area.
 cp -r "$server_root/tests/node/" "$test_path/node";
+
 # Compile the typescript code in test area (src and tests).
 tsc_files=`find $test_path/node -name '*.ts' | tr '\n' ' '`;
 tsc $common_tsc_args --module commonjs $tsc_files;
+
 # Install the npm dependencies
 echo 'Installing NPM dependencies for running the unit tests'
-pushd "$test_path/node";
 # Install the source code dependencies
-npm install .;
+npm install --prefix "$test_path/node";
 # Now run the tests via the jasmine-node runner
-jasmine-node .;
-popd;
+jasmine-node "$test_path/node";
 
 echo 'Testing complete!';
