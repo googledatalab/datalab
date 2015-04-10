98504f2e22e1d32369e03c03c0183276298ca2ea
Drew Bryant <bryantd@google.com>
2015-04-10 14:03:05 -0700

    Adds command line parser to datalab server entry point. Adds auto-browser nav behavior to launcher script

--- tools/datalab-server.sh
+++ tools/datalab-server.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -o errexit; # Fail build on first error, instead of carrying on by default
 
-if [ -z "$REPO_DIR" ];
-  then echo "REPO_DIR is not set. Please run source tools/initenv.sh first";
+if [ -z "$REPO_DIR" ]; then
+  echo "REPO_DIR is not set. Please run source tools/initenv.sh first";
   exit 1;
 fi
 
-if [ -z "$1" ];
-  then echo "Please specify the path to a notebook (.ipynb).";
+if [ -z "$1" ]; then
+  echo "Please specify the path to a notebook (.ipynb).";
   exit 1;
 fi
 
@@ -21,14 +21,24 @@ storage_root=`dirname "$1"`
 notebook_relative_path=`basename $1`;
 
 # Start the DataLab NodeJS server.
-node "$build_path/server.js" "$storage_root" &
+node "$build_path/server.js" -n "$storage_root" &
 # Capture the server process id so that we can kill it later.
 server_pid=$!;
 
 # Wait a moment for server to start before opening a connection.
 sleep 1;
 echo "Opening notebook $notebook_relative_path";
-open "http://localhost:9000/#/notebooks/$notebook_relative_path";
+
+notebook_url="http://localhost:9000/#/notebooks/$notebook_relative_path";
+
+# Open the default browser to the specified notebook path
+echo "Opening your browser to notebook: $notebook_url"
+os=`uname`;
+if [ "$os" == "Darwin" ]; then
+  open "$notebook_url"
+else # Assume Linux
+  xdg-open "$notebook_url"
+fi
 
 # When the user presses Ctrl-C, kill the node server process before exitting.
 trap handle_interrupt INT
