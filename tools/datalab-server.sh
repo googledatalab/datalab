9545556a8c5dd2a3f12c9d063488445f57946cfb
Drew Bryant <bryantd@google.com>
2015-04-10 09:28:48 -0700

    Adds basic DataLab server launcher script for a single notebook path. Opens OS X default browser to notebook URL

---
+++ tools/datalab-server.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -o errexit; # Fail build on first error, instead of carrying on by default
+
+if [ -z "$REPO_DIR" ];
+  then echo "REPO_DIR is not set. Please run source tools/initenv.sh first";
+  exit 1;
+fi
+
+if [ -z "$1" ];
+  then echo "Please specify the path to a notebook (.ipynb).";
+  exit 1;
+fi
+
+# Load the common build config
+source $REPO_DIR/sources/server/config.sh;
+
+# Install NodeJS server dependencies
+npm install --prefix "$build_path";
+
+storage_root=`dirname "$1"`
+notebook_relative_path=`basename $1`;
+
+# Start the DataLab NodeJS server.
+node "$build_path/server.js" "$storage_root" &
+# Capture the server process id so that we can kill it later.
+server_pid=$!;
+
+# Wait a moment for server to start before opening a connection.
+sleep 1;
+echo "Opening notebook $notebook_relative_path";
+open "http://localhost:9000/#/notebooks/$notebook_relative_path";
+
+# When the user presses Ctrl-C, kill the node server process before exitting.
+trap handle_interrupt INT
+function handle_interrupt() {
+  kill $server_pid;
+  echo "Thanks for trying DataLab!";
+}
+
+# Don't exit script unless the node server process dies.
+wait $server_pid;
