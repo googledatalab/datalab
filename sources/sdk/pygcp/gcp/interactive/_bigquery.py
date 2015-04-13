87cba74bcac474a177ca01fa8162d35090f7cff2
Graham Wheeler <gram@google.com>
2015-04-13 14:40:32 -0700

    Made the table view code align more with the schema view code.

--- sources/sdk/pygcp/gcp/interactive/_bigquery.py
+++ sources/sdk/pygcp/gcp/interactive/_bigquery.py
@@ -219,7 +219,7 @@ def _udf_cell(args, js):
 def _table_line(args):
   name = args['table']
   table = _get_table(name)
-  if not table:
+  if not (table and table.exists()):
     print "%s does not exist" % name
   else:
     fields = args['cols'].split(',') if args['cols'] else None
@@ -336,19 +336,6 @@ def _table_viewer(table, rows_per_page=25, job_id='', fields=None):
   return _HTML_TEMPLATE %\
       (left_meta, right_meta, div_id, div_id, table.full_name, ','.join(fields),
        table.length, rows_per_page, _json.dumps(data, cls=_util.JSONEncoder))
-
-
-def _schema_viewer(table, fields=None):
-  """  Return a schema viewer.
-
-  Args:
-    table: the table whose schema should be displayed.
-  Returns:
-    A string containing the HTML for the schema viewer.
-  """
-  if not table.exists():
-    return "<div>%s does not exist</div>" % table.full_name
-  return _repr_html_table_schema(table.schema)
 
 
 def _repr_html_query(query):
