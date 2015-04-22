18c3d8e4fffaf613d12f3f9b4c43c6d81e753221
bryantgipson <bryantgipson@google.com>
2015-04-22 09:39:13 -0700

    Adds build_manifest for quick-and-dirty continuous build verifcation. All calls to build.sh (which ideally should be done before any git push now) will update the build manifest and show any build diffs induced by changes in code or plateform

---
+++ tools/update-build-manifest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$REPO_DIR" ];
+  then echo "REPO_DIR is not set. Please run `source tools/initenv.sh` first";
+  exit 1;
+fi
+
+BUILD_MANIFEST=$REPO_DIR/build_manifest.txt
+
+echo "----build----" > $BUILD_MANIFEST
+find $REPO_DIR/build | sed "s#$REPO_DIR##g" >> $BUILD_MANIFEST
+printf "\n---staging---\n" >> $BUILD_MANIFEST
+find $REPO_DIR/staging | sed "s#$REPO_DIR##g" >> $BUILD_MANIFEST
