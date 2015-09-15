88aff67c152f4c798fd8691a3ea011b5f4050044
Nikhil Kothari <nikhilko@google.com>
2015-09-15 00:44:37 -0700

    License and misc ux related stuff

--- sources/web/datalab/static/datalab.js
+++ sources/web/datalab/static/datalab.js
@@ -126,7 +126,7 @@ function initializePage(dialog) {
       return '/' + path.substr(1).split('/').map(anonymizeString).join('/')
     }
     catch (e) {
-      return '/error';
+      return 'path-error';
     }
   }
 
@@ -135,7 +135,7 @@ function initializePage(dialog) {
       return anonymizeString(document.title || 'untitled');
     }
     catch (e) {
-      return '';
+      return 'title-error';
     }
   }
 
@@ -148,7 +148,9 @@ function initializePage(dialog) {
       '<h5><b>More Information</b></h5>' +
       '<span class="fa fa-external-link-square">&nbsp;</span><a href="https://cloud.google.com" target="_blank">Product information</a><br />' +
       '<span class="fa fa-external-link-square">&nbsp;</span><a href="https://github.com/GoogleCloudPlatform/datalab" target="_blank">Project on GitHub</a><br />' +
-      '<span class="fa fa-external-link-square">&nbsp;</span><a href="/static/about.txt" target="_blank">License and software information</a>';
+      '<span class="fa fa-external-link-square">&nbsp;</span><a href="/static/about.txt" target="_blank">License and software information</a><br />' +
+      '<span class="fa fa-external-link-square">&nbsp;</span><a href="https://cloud.google.com/terms/" target="_blank">Terms of Service</a><br />' +
+      '<span class="fa fa-external-link-square">&nbsp;</span><a href="http://www.google.com/intl/en/policies/" target="_blank">Privacy Policy</a><br />';
 
     var dialogOptions = {
       title: 'About Google Cloud Datalab',
