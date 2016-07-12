2ea7d95633d20eebfc96a076091c1346a68883d5
Omar Jarjur <ojarjur@google.com>
2016-07-12 11:23:29 -0700

    Extend the '/_info' API to include memory info for the container.
    This change adds a 'mem' field to the JSON object returned by the
    '/_info' handler which includes information about the Docker
    container's memory limits/usage.

--- sources/web/datalab/info.ts
+++ sources/web/datalab/info.ts
@@ -15,6 +15,7 @@
 /// <reference path="../../../externs/ts/node/node.d.ts" />
 /// <reference path="common.d.ts" />
 
+import fs = require('fs');
 import http = require('http');
 import jupyter = require('./jupyter');
 import url = require('url');
@@ -25,6 +26,39 @@ import url = require('url');
 var appSettings: common.Settings;
 
 /**
+ * Read one of the 'human readable' files from '/proc' and parse it.
+ *
+ * This assumes that the given 'filename' refers to one of the files under
+ * the '/proc' directory that is meant to be in human readable format. Those
+ * files are formatted as a newline-separated list of key-value pairs (with
+ * the keys and values separated by a ':').
+ *
+ * Since these files are already formatted in a manner that is very similar
+ * to a JSON dictionary, they are trivial to convert to JSON for use in the
+ * '/_info' API.
+ *
+ * @param filename the fully qualified name of the file to read.
+ */
+function readProcFile(filename: string): any {
+  var obj: any = {};
+  try {
+    var contents: string = fs.readFileSync(filename, { encoding: 'utf8' });
+    var lines: Array<string> = contents.split('\n');
+    for (var l in lines) {
+      var line: string = lines[l];
+      var lineParts: Array<string> = line.split(":");
+      if (lineParts.length >= 2) {
+        obj[lineParts[0].trim()] = lineParts[1].trim();
+      }
+    }
+    var lines: Array<string> = contents.split('\n');
+  } catch (e) {
+    return e.message;
+  }
+  return obj;
+}
+
+/**
  * Implements information request handling.
  * @param request the incoming health request.
  * @param response the outgoing health response.
@@ -32,6 +66,7 @@ var appSettings: common.Settings;
 function requestHandler(request: http.ServerRequest, response: http.ServerResponse): void {
   var info: any = {};
   info['env'] = process.env;
+  info['mem'] = readProcFile('/proc/meminfo');
   info['requestHeaders'] = request.headers;
   info['settings'] = appSettings;
   info['servers'] = jupyter.getInfo();
