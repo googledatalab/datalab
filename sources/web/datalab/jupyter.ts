33d495f79ca5402079b76a7a324af3a8f1f49215
corrieann <corrieann@users.noreply.github.com>
2016-11-27 20:24:32 -0800

    When setting the override, don't require an existing access-control-allow-origin header
    Don't require an existing access-control-allow-origin to be set by Jupyter when overriding it from the app config.

--- sources/web/datalab/jupyter.ts
+++ sources/web/datalab/jupyter.ts
@@ -425,15 +425,13 @@ function sendTemplate(key: string, data: common.Map<string>, response: http.Serv
 
 function responseHandler(proxyResponse: http.ClientResponse,
                          request: http.ServerRequest, response: http.ServerResponse) {
-  if (proxyResponse.headers['access-control-allow-origin'] !== undefined) {
-    if (appSettings.allowOriginOverride) {
-      proxyResponse.headers['access-control-allow-origin'] = appSettings.allowOriginOverride;
-    } else {
-      // Delete the allow-origin = * header that is sent (likely as a result of a workaround
-      // notebook configuration to allow server-side websocket connections that are
-      // interpreted by Jupyter as cross-domain).
-      delete proxyResponse.headers['access-control-allow-origin'];
-    }
+  if (appSettings.allowOriginOverride) {
+    proxyResponse.headers['access-control-allow-origin'] = appSettings.allowOriginOverride;
+  } else if (proxyResponse.headers['access-control-allow-origin'] !== undefined) {
+    // Delete the allow-origin = * header that is sent (likely as a result of a workaround
+    // notebook configuration to allow server-side websocket connections that are
+    // interpreted by Jupyter as cross-domain).
+    delete proxyResponse.headers['access-control-allow-origin'];
   }
 
   if (proxyResponse.statusCode != 200) {
