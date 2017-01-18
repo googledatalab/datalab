2857a53979facb79eb8377ca27f5d80bcf15e252
Yasser Elsayed <yasser.ebrahim@gmail.com>
2017-01-17 19:12:06 -0800

    pass env vars to docker container

--- containers/datalab/build.sh
+++ containers/datalab/build.sh
@@ -25,7 +25,17 @@ VERSION_SUBSTITUTION="s/_version_/0.5.$VERSION/"
 COMMIT=`git log --pretty=format:'%H' -n 1`
 COMMIT_SUBSTITUTION="s/_commit_/$COMMIT/"
 
+# Get VM information if running on google cloud
+machine_metadata_url="http://metadata.google.internal/computeMetadata/v1/instance"
+VM_NAME=$(curl -s "${machine_metadata_url}/hostname" -H "Metadata-Flavor: Google" | cut -d '.' -f 1)
+VM_ZONE=$(curl -s "${machine_metadata_url}/zone" -H "Metadata-Flavor: Google" | sed 's/.*zones\///')
+
 cat Dockerfile.in | sed $VERSION_SUBSTITUTION | sed $COMMIT_SUBSTITUTION > Dockerfile
+
+if [ -n "${VM_NAME}" ]; then
+  echo "ENV VM_NAME ${VM_NAME}" >> Dockerfile
+  echo "ENV VM_ZONE ${VM_ZONE}" >> Dockerfile
+fi
 
 # Build the datalab frontend
 source ../../tools/initenv.sh
