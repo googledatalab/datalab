8e4b7a5c2cc2bc90002fe556423469a14a90e081
Chris Meyers <chmeyers@google.com>
2017-04-28 11:33:46 -0700

    Fix merge error.

--- sources/web/datalab/static/notebook-app.js
+++ sources/web/datalab/static/notebook-app.js
@@ -80,6 +80,21 @@ define(['static/appbar', 'static/minitoolbar'], function(appbar, minitoolbar) {
           Jupyter.notebook.undelete_cell.bind(Jupyter.notebook))
     });
 
+    events.on('kernel_connected.Kernel', function() {
+      $('#currentKernelName').text(Jupyter.kernelselector.current_selection);
+      $('#kernelSelectorDropdown').empty();
+      Object.keys(Jupyter.kernelselector.kernelspecs).forEach(function(kernel) {     
+        $('#kernelSelectorDropdown').append(`
+          <li>
+            <a href="#" onclick="Jupyter.kernelselector.set_kernel('` + kernel + `')">
+              ` + kernel + `
+            </a>
+          </li>
+          `
+          )
+      })
+    });
+
     events.on('notebook_loaded.Notebook', function() {
 
       // create the cell toolbar
@@ -657,6 +672,7 @@ define(['static/appbar', 'static/minitoolbar'], function(appbar, minitoolbar) {
       document.getElementById('navigation').style.display = '';
       document.getElementById('help').style.display = 'none';
     }
+
   }
 
   return {
@@ -665,18 +681,5 @@ define(['static/appbar', 'static/minitoolbar'], function(appbar, minitoolbar) {
   };
 });
 
-  events.on('kernel_connected.Kernel', function() {
-    $('#currentKernelName').text(Jupyter.kernelselector.current_selection);
-    $('#kernelSelectorDropdown').empty();
-    Object.keys(Jupyter.kernelselector.kernelspecs).forEach(function(kernel) {     
-      $('#kernelSelectorDropdown').append(`
-        <li>
-          <a href="#" onclick="Jupyter.kernelselector.set_kernel('` + kernel + `')">
-            ` + kernel + `
-          </a>
-        </li>
-        `
-        )
-    })
-  });
-}
+
+
