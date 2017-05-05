googledatalab/datalab

commit f50a4431752b6c4362fee3244af2fb0ad0a89bf9

Convert most files in static to modules. (#1342)
Convert most files in static/ to modules.

sources/web/datalab/static/appbar.js
@@ -1,201 +1,202 @@
-function toggleSidebar() {
-  var d = document.getElementById('sidebarArea');
-  d.style.display = (d.style.display == 'none') ? 'block' : 'none';
-  rotated = $('#hideSidebarButton>.material-icons').css('transform').indexOf('matrix') > -1;
-  $('#hideSidebarButton>.material-icons').css('transform', rotated ? '' : 'rotate(180deg)')
-  this.blur();
-  // Chrome at least seems to render the notebook poorly after this for a little
-  // while. If you scroll new content into view it is messed up until you click
-  // in the notebook. This does not repro with Firefox or Safari so seems to be
-  // a Chrome bug. Triggering a resize or similar doesn't help because the content
-  // that is messed up is currently out of the viewable part of the window. Will
-  // file a bug against Chrome.
-}
+define(() => {
+  function toggleSidebar() {
+    var d = document.getElementById('sidebarArea');
+    d.style.display = (d.style.display == 'none') ? 'block' : 'none';
+    rotated = $('#hideSidebarButton>.material-icons').css('transform').indexOf('matrix') > -1;
+    $('#hideSidebarButton>.material-icons').css('transform', rotated ? '' : 'rotate(180deg)')
+  }
 
-function showHelp(markup) {
-  document.getElementById('navigation').style.display = 'none';
-  document.getElementById('help').style.display = '';
+  function showHelp(markup) {
+    document.getElementById('navigation').style.display = 'none';
+    document.getElementById('help').style.display = '';
 
-  if (markup === undefined)
-    markup = $('#datalabHelp').text();
+    if (markup === undefined)
+      markup = $('#datalabHelp').text();
+    $('#help').html(markup);
+
+    if (document.getElementById('sidebarArea').style.display == 'none') {
+      toggleSidebar();
+    }
+  }
+
+  // Populate help for the first time sidebar is opened
+  markup = $('#datalabHelp').text();
   $('#help').html(markup);
 
-  if (document.getElementById('sidebarArea').style.display == 'none') {
-    toggleSidebar();
-  }
-}
+  function restartDatalab() {
+    var restartUrl = window.location.protocol + "//" + window.location.host + "/_restart";
 
-// Populate help for the first time sidebar is opened
-markup = $('#datalabHelp').text();
-$('#help').html(markup);
-
-function restartDatalab() {
-  var restartUrl = window.location.protocol + "//" + window.location.host + "/_restart";
-
-  function redirect() {
-    window.location = '/';
-  }
-
-  xhr(restartUrl, function(){
-    // We redirect to signal to the user that the restart did something.
-    // However, we have to delay that a bit to give Datalab time to restart.  
-    window.setTimeout(redirect, 500);
-  }, "POST");
-}
-
-function getVmInfo(callback) {
-  if (window.datalab.vminfo) {
-    callback(window.datalab.vminfo);
-    return;
-  }
-
-  path = window.location.protocol + '//' + window.location.host + '/_info/vminfo';
-  xhr(path, function() {
-    try {
-      vminfo = JSON.parse(this.responseText);
-      window.datalab.vminfo = vminfo;
-      callback(vminfo);
-    } catch(e) {
-      callback(null);
+    function redirect() {
+      window.location = '/';
     }
-  });
-}
 
-function manageVm() {
-  getVmInfo(function(vminfo) {
-    if (vminfo && vminfo.vm_name && vminfo.vm_zone) {
-      window.open('https://console.cloud.google.com/compute/instancesDetail' +
-          '/zones/' + vminfo.vm_zone +
-          '/instances/' + vminfo.vm_name +
-          '?project=' + vminfo.vm_project);
-    } else {
-      console.log('Error, could not retrieve VM information. Is this a google cloud VM?');
-    }
-  });
-}
-
-function stopVm() {
-  let action = confirm('Stopping this VM will discard any unsaved state. Are you sure?');
-  if (action === true) {
-    path = window.location.protocol + '//' + window.location.host + '/_stopvm';
-    xhr(path, null, 'POST');
-  }
-}
-
-function initializeAppBar(dialog, saveFn) {
-
-  function showAbout() {
-    var version = document.body.getAttribute('data-version-id');
-    var reportingEnabled = (document.body.getAttribute('data-reporting-enabled') == 'true');
-    var dialogContent =
-      '<p>Interactive notebooks, with Python and SQL on Google Cloud Platform.</p>' +
-      '<p>Explore, transform, visualize and process data using BigQuery and Google Cloud Storage.</p><br />' +
-      '<pre>Version: ' + version  + '\nBased on Jupyter (formerly IPython) 4</pre>' +
-      '<h5><b>More Information</b></h5>' +
-      '<i class="material-icons">open_in_new</i><a href="https://cloud.google.com" target="_blank"> Product information</a><br />' +
-      '<i class="material-icons">open_in_new</i><a href="https://github.com/googledatalab/datalab" target="_blank"> Project on GitHub</a><br />' +
-      '<i class="material-icons">open_in_new</i><a href="/static/about.txt" target="_blank"> License and software information</a><br />' +
-      '<i class="material-icons">open_in_new</i><a href="https://cloud.google.com/terms/" target="_blank"> Terms of Service</a><br />' +
-      '<i class="material-icons">open_in_new</i><a href="http://www.google.com/intl/en/policies/" target="_blank"> Privacy Policy</a><br />' +
-      '<i class="material-icons">open_in_new</i><a href="javascript:restartDatalab()"> Restart Server</a><br />';
-
-    var dialogOptions = {
-      title: 'About Google Cloud Datalab',
-      body: $(dialogContent),
-      buttons: { 'OK': {} }
-    };
-    dialog.modal(dialogOptions);
+    xhr(restartUrl, function(){
+      // We redirect to signal to the user that the restart did something.
+      // However, we have to delay that a bit to give Datalab time to restart.  
+      window.setTimeout(redirect, 500);
+    }, "POST");
   }
 
-  // Prepare sign in/out UI
-  $('#accountDropdownButton').on('click', function (event) {
-    $(this).parent().toggleClass('open');
-    if (window.datalab && window.datalab.session) {
-      window.datalab.session.execute("datalab_project_id()", function(error, projectId) {
-        if (error === null || error === undefined) {
-          $('#projectLabel').text("Active project: " + projectId);
-          $('#projectLabel').show();
-        }
-      });
+  function getVmInfo(callback) {
+    if (window.datalab.vminfo) {
+      callback(window.datalab.vminfo);
+      return;
     }
-  });
-  $('body').on('click', function (e) {
-    if (!$('#accountDropdown').is(e.target)
-        && $('#accountDropdown').has(e.target).length === 0
-        && $('.open').has(e.target).length === 0
-    ) {
-        $('#accountDropdown').removeClass('open');
-    }
-  });
-  var signedIn = document.body.getAttribute('data-signed-in');
-  if (signedIn != undefined) {
-    if (signedIn == "true") {
-      $('#signOutGroup').show();
-      var username = document.body.getAttribute('data-account');
-      $("#usernameLabel").text("Signed in as " + username);
-      if (username.indexOf('gserviceaccount.com') < 0) {
-        $('#signOutButton').show();
+
+    path = window.location.protocol + '//' + window.location.host + '/_info/vminfo';
+    xhr(path, function() {
+      try {
+        vminfo = JSON.parse(this.responseText);
+        window.datalab.vminfo = vminfo;
+        callback(vminfo);
+      } catch(e) {
+        callback(null);
       }
-    } else {
-      $('#signInButton').show();
-    }
-    $('#signInButton').click(function() {
-      saveFn();
-      window.location = '/signin?referer=' + encodeURIComponent(window.location);
-    });
-    $('#signOutButton').click(function() {
-      saveFn();
-      window.location = '/signout?referer=' + encodeURIComponent(window.location);
-    });
-    $('#ungitButton').click(function() {
-      // Always open at the root of the notebooks repository
-      const path = '/content/datalab/notebooks';
-      const prefix = window.location.protocol + '//' + window.location.host;
-
-      window.open(prefix + '/_proxy/8083/#/repository?path=' + path);
     });
   }
 
-  getVmInfo(function() {
-    if (vminfo && vminfo.vm_name) {
-      $('#stopVmGroup').show();
-      $('#vmName').text(vminfo.vm_name);
-      $('#stopVmButton').click(stopVm);
-    }
-  });
-
-  // If inside a notebook, prepare notebook-specific help link inside the sidebar
-  if (document.getElementById('sidebarArea') !== null) {
-    $('#showHelpLink').click(function(e) {
-      showHelp(document.getElementById('datalabHelp').textContent);
-      e.preventDefault();
+  function manageVm() {
+    getVmInfo(function(vminfo) {
+      if (vminfo && vminfo.vm_name && vminfo.vm_zone) {
+        window.open('https://console.cloud.google.com/compute/instancesDetail' +
+            '/zones/' + vminfo.vm_zone +
+            '/instances/' + vminfo.vm_name +
+            '?project=' + vminfo.vm_project);
+      } else {
+        console.log('Error, could not retrieve VM information. Is this a google cloud VM?');
+      }
     });
-    $('#showHelpLink').show()
-    $('#keyboardHelpLink').click(function(e) {
-      showHelp(document.getElementById('shortcutsHelp').textContent);
-      e.preventDefault();
-    });
-    $('#keyboardHelpLink').show()
-    $('#markdownHelpLink').click(function(e) {
-      showHelp(document.getElementById('markdownHelp').textContent);
-      e.preventDefault();
-    });
-    $('#markdownHelpLink').show()
-    $('#notebookHelpDivider').show()
-
-    $('#navigationButton').show()
   }
-  $('#aboutButton').click(showAbout);
-  $('#settingsButton').click(function() {
-    $.get('/static/settings.html', (markup) => {
-      dialog.modal({
-        title: 'Settings',
-        body: $(markup),
-        buttons: { Close: {} }
+
+  function stopVm() {
+    let action = confirm('Stopping this VM will discard any unsaved state. Are you sure?');
+    if (action === true) {
+      path = window.location.protocol + '//' + window.location.host + '/_stopvm';
+      xhr(path, null, 'POST');
+    }
+  }
+
+  function initializeAppBar(dialog, saveFn) {
+
+    function showAbout() {
+      var version = document.body.getAttribute('data-version-id');
+      var reportingEnabled = (document.body.getAttribute('data-reporting-enabled') == 'true');
+      var dialogContent =
+        '<p>Interactive notebooks, with Python and SQL on Google Cloud Platform.</p>' +
+        '<p>Explore, transform, visualize and process data using BigQuery and Google Cloud Storage.</p><br />' +
+        '<pre>Version: ' + version  + '\nBased on Jupyter (formerly IPython) 4</pre>' +
+        '<h5><b>More Information</b></h5>' +
+        '<i class="material-icons">open_in_new</i><a href="https://cloud.google.com" target="_blank"> Product information</a><br />' +
+        '<i class="material-icons">open_in_new</i><a href="https://github.com/googledatalab/datalab" target="_blank"> Project on GitHub</a><br />' +
+        '<i class="material-icons">open_in_new</i><a href="/static/about.txt" target="_blank"> License and software information</a><br />' +
+        '<i class="material-icons">open_in_new</i><a href="https://cloud.google.com/terms/" target="_blank"> Terms of Service</a><br />' +
+        '<i class="material-icons">open_in_new</i><a href="http://www.google.com/intl/en/policies/" target="_blank"> Privacy Policy</a><br />' +
+        '<i class="material-icons">open_in_new</i><a href="javascript:restartDatalab()"> Restart Server</a><br />';
+
+      var dialogOptions = {
+        title: 'About Google Cloud Datalab',
+        body: $(dialogContent),
+        buttons: { 'OK': {} }
+      };
+      dialog.modal(dialogOptions);
+    }
+
+    // Prepare sign in/out UI
+    $('#accountDropdownButton').on('click', function (event) {
+      $(this).parent().toggleClass('open');
+      if (window.datalab && window.datalab.session) {
+        window.datalab.session.execute("datalab_project_id()", function(error, projectId) {
+          if (error === null || error === undefined) {
+            $('#projectLabel').text("Active project: " + projectId);
+            $('#projectLabel').show();
+          }
+        });
+      }
+    });
+    $('body').on('click', function (e) {
+      if (!$('#accountDropdown').is(e.target)
+          && $('#accountDropdown').has(e.target).length === 0
+          && $('.open').has(e.target).length === 0
+      ) {
+          $('#accountDropdown').removeClass('open');
+      }
+    });
+    var signedIn = document.body.getAttribute('data-signed-in');
+    if (signedIn != undefined) {
+      if (signedIn == "true") {
+        $('#signOutGroup').show();
+        var username = document.body.getAttribute('data-account');
+        $("#usernameLabel").text("Signed in as " + username);
+        if (username.indexOf('gserviceaccount.com') < 0) {
+          $('#signOutButton').show();
+        }
+      } else {
+        $('#signInButton').show();
+      }
+      $('#signInButton').click(function() {
+        saveFn();
+        window.location = '/signin?referer=' + encodeURIComponent(window.location);
+      });
+      $('#signOutButton').click(function() {
+        saveFn();
+        window.location = '/signout?referer=' + encodeURIComponent(window.location);
+      });
+      $('#ungitButton').click(function() {
+        // Always open at the root of the notebooks repository
+        const path = '/content/datalab/notebooks';
+        const prefix = window.location.protocol + '//' + window.location.host;
+
+        window.open(prefix + '/_proxy/8083/#/repository?path=' + path);
+      });
+    }
+
+    getVmInfo(function() {
+      if (vminfo && vminfo.vm_name) {
+        $('#stopVmGroup').show();
+        $('#vmName').text(vminfo.vm_name);
+        $('#stopVmButton').click(stopVm);
+      }
+    });
+
+    // If inside a notebook, prepare notebook-specific help link inside the sidebar
+    if (document.getElementById('sidebarArea') !== null) {
+      $('#showHelpLink').click(function(e) {
+        showHelp(document.getElementById('datalabHelp').textContent);
+        e.preventDefault();
+      });
+      $('#showHelpLink').show()
+      $('#keyboardHelpLink').click(function(e) {
+        showHelp(document.getElementById('shortcutsHelp').textContent);
+        e.preventDefault();
+      });
+      $('#keyboardHelpLink').show()
+      $('#markdownHelpLink').click(function(e) {
+        showHelp(document.getElementById('markdownHelp').textContent);
+        e.preventDefault();
+      });
+      $('#markdownHelpLink').show()
+      $('#notebookHelpDivider').show()
+
+      $('#navigationButton').show()
+    }
+    $('#aboutButton').click(showAbout);
+    $('#settingsButton').click(function() {
+      $.get('/static/settings.html', (markup) => {
+        dialog.modal({
+          title: 'Settings',
+          body: $(markup),
+          buttons: { Close: {} }
+        });
       });
     });
-  });
-  $('#feedbackButton').click(function() {
-    window.open('https://groups.google.com/forum/#!newtopic/google-cloud-datalab-feedback');
-  });
-}
+    $('#feedbackButton').click(function() {
+      window.open('https://groups.google.com/forum/#!newtopic/google-cloud-datalab-feedback');
+    });
+  }
+
+  return {
+    init: initializeAppBar,
+    showHelp: showHelp,
+    toggleSidebar: toggleSidebar,
+  };
+});

sources/web/datalab/static/datalab.js
@@ -47,13 +47,16 @@ function xhr(url, callback, method) {
   request.send();
 }
 
-function initializeDataLab(ipy, events, dialog, utils, security) {
+function initializeDataLab(
+    ipy, events, dialog, utils, security, appbar, editapp,
+    notebookapp, notebooklist
+  ) {
   var saveFn = function() {
     if (('notebook' in ipy) && ipy.notebook) {
       ipy.notebook.save_checkpoint();
     }
   }
-  initializeAppBar(dialog, saveFn);
+  appbar.init(dialog, saveFn);
 
   // Override the sanitizer - all notebooks within the user's volume are implicity
   // trusted, and there is no need to remove scripts from cell outputs of notebooks
@@ -64,21 +67,21 @@ function initializeDataLab(ipy, events, dialog, utils, security) {
 
   var pageClass = document.body.className;
   if (pageClass.indexOf('notebook_app') >= 0) {
-    initNotebookApplication_preLoad(ipy, ipy.notebook, events, dialog, utils);
+    notebookapp.preLoad(ipy, ipy.notebook, events, dialog, utils);
     events.on('notebook_loaded.Notebook', function() {
-      initNotebookApplication_postLoad(ipy, ipy.notebook, events, dialog, utils);
+      notebookapp.postLoad(ipy, ipy.notebook, events, dialog, utils);
       window.datalab.loaded = true;
     });
   }
   else if (pageClass.indexOf('edit_app') >= 0) {
     events.on('file_loaded.Editor', function() {
-      initEditApplication_postLoad(ipy, ipy.editor);
+      editapp.postLoad(ipy, ipy.editor);
       window.datalab.loaded = true;
     });
   }
   else if (pageClass.indexOf('notebook_list') >= 0) {
     events.on('draw_notebook_list.NotebookList', function() {
-      initNotebookList_postLoad(ipy, ipy.notebook_list, ipy.new_notebook_widget,
+      notebooklist.postLoad(ipy, ipy.notebook_list, ipy.new_notebook_widget,
                              events, dialog, utils);
       window.datalab.loaded = true;
     });
@@ -98,11 +101,13 @@ define([
   'base/js/security',
   'static/appbar',
   'static/edit-app',
-  'static/minitoolbar',
   'static/notebook-app',
   'static/notebook-list',
+  'static/minitoolbar',
   'static/websocket'
 ], function(ipy, events, dialog, utils, security, appbar, editapp,
-            minitoolbar, notebookapp, notebooklist, websocket) {
-  initializeDataLab(IPython, events, dialog, utils, security);
+            notebookapp, notebooklist, minitoolbar, websocket) {
+     initializeDataLab(
+            ipy, events, dialog, utils, security, appbar, editapp,
+            notebookapp, notebooklist);
 });

sources/web/datalab/static/edit-app.js
@@ -1,30 +1,36 @@
-function initEditApplication_postLoad(ipy, editor) {
-  function navigateAlternate(alt) {
-    var url = document.location.href.replace('/edit', alt);
-    if (url.includes("?")) {
-      url = url.slice(0, url.lastIndexOf("?"));
-    }
-    url = url + '?download=true';
+define(() => {
+  function postLoad(ipy, editor) {
+    function navigateAlternate(alt) {
+      var url = document.location.href.replace('/edit', alt);
+      if (url.includes("?")) {
+        url = url.slice(0, url.lastIndexOf("?"));
+      }
+      url = url + '?download=true';
 
-    if (!editor.clean) {
-      editor.save().then(function() {
+      if (!editor.clean) {
+        editor.save().then(function() {
+          window.open(url);
+        });
+      }
+      else {
         window.open(url);
-      });
+      }
     }
-    else {
-      window.open(url);
-    }
+
+    $('#saveButton').click(function() {
+      editor.save();
+    })
+
+    $('#renameButton').click(function() {
+      Jupyter.notebook.save_widget.rename();
+    })
+
+    $('#downloadButton').click(function() {
+      navigateAlternate('/files');
+    })
   }
 
-  $('#saveButton').click(function() {
-    editor.save();
-  })
-
-  $('#renameButton').click(function() {
-    Jupyter.notebook.save_widget.rename();
-  })
-
-  $('#downloadButton').click(function() {
-    navigateAlternate('/files');
-  })
-}
+  return {
+    postLoad: postLoad
+  };
+});

commit e2279faa7b05c431541804061eaef5e00dc052b5

Fix restart-server (broken in #1342).

sources/web/datalab/static/appbar.js
@@ -91,7 +91,7 @@ define(() => {
         '<i class="material-icons">open_in_new</i><a href="/static/about.txt" target="_blank"> License and software information</a><br />' +
         '<i class="material-icons">open_in_new</i><a href="https://cloud.google.com/terms/" target="_blank"> Terms of Service</a><br />' +
         '<i class="material-icons">open_in_new</i><a href="http://www.google.com/intl/en/policies/" target="_blank"> Privacy Policy</a><br />' +
-        '<i class="material-icons">open_in_new</i><a href="javascript:restartDatalab()"> Restart Server</a><br />';
+        '<i class="material-icons">open_in_new</i><a href="javascript:require([\'appbar\'],appbar=>appbar.restartDatalab())"> Restart Server</a><br />';
 
       var dialogOptions = {
         title: 'About Google Cloud Datalab',
@@ -196,6 +196,7 @@ define(() => {
 
   return {
     init: initializeAppBar,
+    restartDatalab: restartDatalab,
     showHelp: showHelp,
     toggleSidebar: toggleSidebar,
   };