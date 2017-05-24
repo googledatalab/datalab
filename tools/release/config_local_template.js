40c029fa95f9da23fcc676d4109304294c244d0b
Yasser Elsayed <yelsayed@google.com>
2017-05-24 10:51:48 -0700

    fix check for version array length

--- tools/release/config_local_template.js
+++ tools/release/config_local_template.js
@@ -1,8 +1,9 @@
 const vid = document.body.getAttribute("data-version-id");
 const GTM_ACCOUNT = {{GTM_ACCOUNT_PLACEHOLDER}};
 const LATEST_SEMVER = {{DATALAB_VERSION_PLACEHOLDER}};
-const latest = vid.split('.')[2] < "20170523" ? {{DATALAB_VERSION_PATCH_PLACEHOLDER}} : LATEST_SEMVER;
-if (latest) {
+const version_tokens = vid.split('.');
+if (version_tokens.length === 3) {
+  const latest = version_tokens[2] < "20170523" ? {{DATALAB_VERSION_PATCH_PLACEHOLDER}} : LATEST_SEMVER;
   const LAST_SEMVER = "0.5.20160802";
   const PREV_SEMVER = {{PREV_SEMVER_PLACEHOLDER}};
 
