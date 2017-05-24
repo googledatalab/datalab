e05f065745846881e5c56c01c53fa69fc9fc42f7
Yasser Elsayed <yelsayed@google.com>
2017-05-24 10:28:33 -0700

    validate current version

--- tools/release/config_local_template.js
+++ tools/release/config_local_template.js
@@ -1,15 +1,17 @@
 const vid = document.body.getAttribute("data-version-id");
+const GTM_ACCOUNT = {{GTM_ACCOUNT_PLACEHOLDER}};
 const LATEST_SEMVER = {{DATALAB_VERSION_PLACEHOLDER}};
 const latest = vid.split('.')[2] < "20170523" ? {{DATALAB_VERSION_PATCH_PLACEHOLDER}} : LATEST_SEMVER;
-const LAST_SEMVER = "20160802";
-const PREV_SEMVER = {{PREV_SEMVER_PLACEHOLDER}};
-const GTM_ACCOUNT = {{GTM_ACCOUNT_PLACEHOLDER}};
+if (latest) {
+  const LAST_SEMVER = "0.5.20160802";
+  const PREV_SEMVER = {{PREV_SEMVER_PLACEHOLDER}};
 
-window.datalab.versions = {
-  latest:   latest,
-  last:     LAST_SEMVER,
-  previous: PREV_SEMVER,
-};
+  window.datalab.versions = {
+    latest:   latest,
+    last:     LAST_SEMVER,
+    previous: PREV_SEMVER,
+  };
+}
 
 window.datalab.gtmAccount = GTM_ACCOUNT;
 window.dataLayer = window.dataLayer || [];
