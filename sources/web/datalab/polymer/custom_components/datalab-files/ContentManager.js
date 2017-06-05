890f3e4acce4861df1ed6cdefb26794c682133fd
Yasser Elsayed <yasser.ebrahim@gmail.com>
2017-06-04 17:08:27 -0700

    use absolute heights in preparation for item-list element

--- sources/web/datalab/polymer/custom_components/datalab-files/ContentManager.js
+++ sources/web/datalab/polymer/custom_components/datalab-files/ContentManager.js
@@ -1,4 +1,48 @@
+// Sends an XMLHttpRequest to the specified URL.
+// Options can contain the following fields:
+//   method: The HTTP method to use; default is 'GET'
+//   errorCallback: A function to call if the XHR completes
+//       with a status other than 200
+function xhr(url, callback, options) {
+  options = options || {};
+  const method = options.method || 'GET';
+
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        if (callback) {
+          callback(request);
+        }
+      } else {
+        if (options.errorCallback) {
+          options.errorCallback.call(request);
+        }
+      }
+    }
+  }
+  request.open(method, url);
+  request.send();
+}
+
 class ContentManager {
-  static listFiles(path) {
+
+  static apiUrl() {
+    return '/api/contents';
+  }
+
+  static listFilesAsync(path) {
+    const listPromise = new Promise((resolve, reject) => {
+      xhr(this.apiUrl() + path,
+          request => {
+            resolve(request.response);
+          },
+          error => {
+            reject('Could not contact endpoint: ' + this.apiUrl());
+          }
+      );
+    });
+
+    return listPromise;
   }
 }
--- sources/web/datalab/polymer/custom_components/datalab-files/datalab-files.js
+++ sources/web/datalab/polymer/custom_components/datalab-files/datalab-files.js
@@ -8,12 +8,47 @@ class FilesElement extends Polymer.Element {
         type: String,
         value: '/',
         observer: '_currentPathChanged'
+      },
+      fileList: {
+        type: Object,
+        value: {},
+        observer: '_fileListChanged'
       }
     }
   }
 
+  _getFileListElement() {
+    return this.$.files;
+  }
+
   _currentPathChanged(newValue) {
-    ContentManager.listFiles('/');
+    const self = this;
+    ContentManager.listFilesAsync(this.currentPath)
+      .then(list => {
+        try {
+          list = JSON.parse(list);
+          self.fileList = list;
+        } catch(e) {
+          console.log('Could not get list of files');
+        }
+      });
+  }
+
+  _fileListChanged() {
+    // initial undefined value
+    if (!this.fileList.content) {
+      return;
+    }
+
+    const picker = this._getFileListElement();
+    let newList = [];
+    this.fileList.content.forEach(file => {
+      newList.push({
+        'name': file.name,
+        'status': ''
+      });
+    });
+    picker.rows = newList;
   }
 
 }
