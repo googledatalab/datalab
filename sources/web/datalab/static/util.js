a5e069c34978fe9084e31c85a3cb668a0623864e
Omar Jarjur <ojarjur@google.com>
2017-06-08 14:05:56 -0700

    Put the fields of the utils package in alphabetical order

--- sources/web/datalab/static/util.js
+++ sources/web/datalab/static/util.js
@@ -1,4 +1,23 @@
 define(() => {
+  // Generate an absolute link to a path on this same Datalab instance.
+  function datalabLink(fullPath) {
+    var basePath = document.body.getAttribute('data-base-url');
+    if (fullPath.indexOf('/') == 0) {
+      fullPath = basePath + fullPath.slice(1);
+    }
+    return window.location.protocol + '//' + window.location.host + fullPath;
+  }
+
+  // Return the portion of the given full path that is
+  // relative to the Datalab base path.
+  function datalabSubPath(fullPath) {
+    var basePath = document.body.getAttribute('data-base-url');
+    if (fullPath.indexOf(basePath) == 0) {
+      return '/' + fullPath.slice(basePath.length);
+    }
+    return fullPath;
+  }
+
   const debug = {
     log: function() {
       if (window.location.search.indexOf('debug=true') > -1) {
@@ -49,30 +68,11 @@ define(() => {
     request.send();
   }
 
-  // Generate an absolute link to a path on this same Datalab instance.
-  function datalabLink(fullPath) {
-    var basePath = document.body.getAttribute('data-base-url');
-    if (fullPath.indexOf('/') == 0) {
-      fullPath = basePath + fullPath.slice(1);
-    }
-    return window.location.protocol + '//' + window.location.host + fullPath;
-  }
-
-  // Return the portion of the given full path that is
-  // relative to the Datalab base path.
-  function datalabSubPath(fullPath) {
-    var basePath = document.body.getAttribute('data-base-url');
-    if (fullPath.indexOf(basePath) == 0) {
-      return '/' + fullPath.slice(basePath.length);
-    }
-    return fullPath;
-  }
-
   return {
+    datalabLink,
+    datalabSubPath,
     debug,
     reportEvent,
     xhr,
-    datalabLink,
-    datalabSubPath,
   };
 });
