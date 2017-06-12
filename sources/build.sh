5354657e8c022a9a0d9fe4ad73489493d3ba12d5
Yasser Elsayed <yebrahim@users.noreply.github.com>
2017-06-12 15:27:33 -0700

    Auto install bower and polymer-cli when building source dir (#1423)
    Also fix the build script to correctly detect npm installation, and use ambient npm instead of explicit path npm. Additionally, npm installation now is done under datalab's tools dir.
    
    * auto install bower and polymer-cli when building source dir
    
    * test once for npm

--- sources/build.sh
+++ sources/build.sh
@@ -24,18 +24,26 @@ function install_node() {
   wget --version || install_wget
 
   echo "Installing NodeJS"
-  mkdir -p /tools/node
+  mkdir -p ./tools/node
   wget -nv https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.gz -O node.tar.gz
-  tar xzf node.tar.gz -C /tools/node --strip-components=1
+  tar xzf node.tar.gz -C ./tools/node --strip-components=1
   rm node.tar.gz
-  export "PATH=${PATH}:/tools/node/bin"
+  export "PATH=${PATH}:${PWD}/tools/node/bin"
+}
+
+function install_bower() {
+  echo "Installing Bower"
+  npm install -g bower
+}
+
+function install_polyer_cli() {
+  echo "Installing Polymer CLI"
+  npm install -g polymer-cli
 }
 
 function install_typescript() {
-  npm -h >/dev/null 2>&1 || install_node
-
   echo "Installing Typescript"
-  /tools/node/bin/npm install -g typescript
+  npm install -g typescript
 }
 
 function install_git() {
@@ -55,7 +63,11 @@ function install_prereqs() {
   git version || install_git
   rsync -h >/dev/null 2>&1 || install_rsync
 
+  # Use -v instead of -h to test npm installation, since -h returns non-zero
+  npm -v >/dev/null 2>&1 || install_node
   tsc -h >/dev/null 2>&1  || install_typescript
+  bower -h >/dev/null 2>&1  || install_bower
+  polymer -h >/dev/null 2>&1  || install_polyer_cli
   source ./tools/initenv.sh
 }
 
