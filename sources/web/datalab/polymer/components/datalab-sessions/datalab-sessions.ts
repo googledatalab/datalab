55ef794dd333333895d0c651f200f098cd226eb1
Yasser Elsayed <yebrahim@users.noreply.github.com>
2017-06-13 23:52:47 -0700

    Flesh out datalab-files and datalab-sessions for Polymer UI (#1424)
    This change adds content to the datalab-files and datalab-sessions elements, and adds an ApiManager javascript module that these two use to load files and sessions from the Jupyter server. There's also a new element <item-list>, which renders the data in two columns.
    
    * init polymer ui redo
    
    * basic toolbar and sidebar
    
    * add selected attributes for sidebar and button
    
    * rename toolbar-button to sidebar-button
    
    * use app-layout elements instead of paper-toolbar
    
    * wrap everything in datalab-app element
    
    * move polymer files into polymer-redo dir
    
    * hook up click events in sidebar-button and datalab-sidebar
    
    * strip down the sidebar to only the list of buttons, so it can be embedded anywhere
    
    * hook up iron-pages and switch pages on client
    
    * get rid of sidebar-button, use iron-selector, and set up client routing
    
    * add svg logo
    
    * added settings pane
    
    * fix css issues with scrolling
    
    * added help dialog
    
    * move dialogs to top-level to avoid stacking context trap
    
    * add iron-pages for file services
    
    * add missing bower dependency
    
    * using tabs instead of iron-selector, styling
    
    * started on new design
    
    * sidebar fixes to match new design, shared styles
    
    * add file list header and toolbar
    
    * adding polymer build configs
    
    * move paper-dialogs into toolbar element, style fixes
    
    * move polymer inside web sources directory
    
    * start serving polymer scripts from datalab
    
    * fix build break and load both old and new interfaces
    
    * started on content manager
    
    * fix idletimeout issues
    
    * use absolute heights in preparation for item-list element
    
    * initial item-list element, with multi-selection and highlight
    
    * add striping and hover to item-list
    
    * add navigation to datalab-files, still need backward navigation
    
    * added nav icons, breadcrumbs next
    
    * working breadcrumbs
    
    * working backward/forward nav
    
    * added progress bar, thinner breadcrumb borders
    
    * add folder/file icon to list, color fixes
    
    * clicking opens notebooks, + refresh button, refresh every 10s
    
    * generalize item-list for both files and sessions
    
    * working sessions page, using ApiManager
    
    * added an env var for experimental ui
    
    * redirect base path to /files
    
    * cleanup
    
    * added simple polymer ts definition file
    
    * cleanup
    
    * adding experimental to gitignore
    
    * first element in typescript: datalab-app, other ts cleanup
    
    * datalab-toolbar is now in typescript
    
    * datalab-sidebar is now in typescript
    
    * item-list is now in typescript
    
    * started on ApiManager ts conversion
    
    * rename custom_elements to components
    
    * datalab-files and datalab-sessions are now in typescript
    
    * complete migration to typescript
    
    * merge with master
    
    * added blank details pane
    
    * added few details to details pane
    
    * remove details pane
    
    * cleanup and comments
    
    * more cleanup and comments, fix nav buttons disabled style
    
    * disable experimental ui by default

--- sources/web/datalab/polymer/components/datalab-sessions/datalab-sessions.ts
+++ sources/web/datalab/polymer/components/datalab-sessions/datalab-sessions.ts
@@ -12,8 +12,109 @@
  * the License.
  */
 
+/// <reference path="../../modules/ApiManager.ts" />
+/// <reference path="../item-list/item-list.ts" />
+
+/**
+ * Session listing element for Datalab.
+ * Contains an item-list element to display sessions, a toolbar to interact with these sessions,
+ * a progress bar that appears while loading the list
+ */
 class SessionsElement extends Polymer.Element {
+
+  private _sessionList: Array<Session>;
+  private _fetching: boolean;
+  private _sessionListRefreshInterval: number;
+
   static get is() { return "datalab-sessions"; }
+
+  static get properties() {
+    return {
+      _sessionList: {
+        type: Array,
+        value: function() {
+          return [];
+        },
+      },
+      _fetching: {
+        type: Boolean,
+        value: false
+      },
+      _sessionListRefreshInterval: {
+        type: Number,
+        value: 10000
+      }
+    }
+  }
+
+  /**
+   * Called when when the element's local DOM is ready and initialized We use it
+   * to initialize element state.
+   */
+  ready() {
+    super.ready();
+
+    this.$.sessions.columns = ['Session Path', 'Status'];
+
+    // load session list initially
+    this._fetchSessionList();
+
+    // Refresh the session list periodically.
+    // TODO: [yebrahim] Start periodic refresh when the window is in focus, and
+    // the sessions page is open, and stop it on blur to minimize unnecessary traffic
+    setInterval(this._fetchSessionList.bind(this), this._sessionListRefreshInterval);
+  }
+
+  /**
+   * Creates a new ItemListRow object for each entry in the session list, and sends
+   * the created list to the item-list to render.
+   */
+  _drawSessionList() {
+    // initial value
+    if (!Array.isArray(this._sessionList)) {
+      return;
+    }
+
+    this.$.sessions.rows = this._sessionList.map(session => {
+      return {
+        firstCol: session.notebook.path,
+        secondCol: 'running',
+        icon: 'editor:insert-drive-file',
+        selected: false
+      };
+    });
+  }
+
+  /**
+   * Calls the ApiManager to get the list of sessions at the current path, and
+   * updates the _sessionList property.
+   */
+  _fetchSessionList() {
+    const self = this;
+    self._fetching = true;
+    return ApiManager.listSessionsAsync()
+      .then(newList => {
+        // Only refresh the list if there are any changes. This helps keep
+        // the item list's selections intact most of the time
+        // TODO: [yebrahim] Try to diff the two lists and only inject the
+        // differences in the DOM instead of refreshing the entire list if
+        // one item changes. This is tricky because we don't have unique
+        // ids for the items. Using paths might work for files, but is not
+        // a clean solution.
+        if (JSON.stringify(this._sessionList) !== JSON.stringify(newList)) {
+          self._sessionList = newList;
+          this._drawSessionList();
+        }
+      }, () => {
+        // TODO: [yebrahim] Add dummy data here when debugging is enabled to allow for
+        // fast UI iteration using `polymer serve`.
+        console.log('Error getting list of sessions.');
+      })
+      .then(() => {
+        this._fetching = false;
+      });
+  }
+
 }
 
 customElements.define(SessionsElement.is, SessionsElement);
