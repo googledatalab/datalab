9d22b902fceb77083053e6fcde763cc23dd245b0
Jim McBeath <jimmc@google.com>
2017-08-04 11:17:54 -0700

    Move utility methods into TimeoutManager, add unit tests.

--- sources/web/datalab/polymer/components/timeout-panel/timeout-panel.ts
+++ sources/web/datalab/polymer/components/timeout-panel/timeout-panel.ts
@@ -28,11 +28,6 @@
 class TimeoutPanel extends Polymer.Element {
 
   private static _displayInterval = 1000;  // Update display every second
-  private static _hoursPerDay = 24;
-  private static _minutesPerHour = 60;
-  private static _secondsPerMinute = 60;
-  private static _secondsPerHour = TimeoutPanel._secondsPerMinute * TimeoutPanel._minutesPerHour;
-  private static _secondsPerDay = TimeoutPanel._secondsPerHour * TimeoutPanel._hoursPerDay;
   private static _queryIntervalWhenEnabled = 15 * 1000;  // How often to query when timeout is enabled
   private static _queryIntervalWhenDisabled = 60 * 1000;  // How often to query when timeout is disabled
 
@@ -101,79 +96,16 @@ class TimeoutPanel extends Polymer.Element {
       if (secondsRemaining < 0) {
         secondsRemaining = 0;
       }
-      const roundedSecondsRemaining = this._roundToApproximateTime(secondsRemaining);
+      const roundedSecondsRemaining = TimeoutManager.roundToApproximateTime(secondsRemaining);
       const maybeAbout = (roundedSecondsRemaining !== secondsRemaining) ? 'about ' : '';
       this._timeoutText = (roundedSecondsRemaining === 0) ?
           'Idle timeout exceeded' :
-          'Idle timeout in ' + maybeAbout + this._secondsToString(roundedSecondsRemaining);
+          'Idle timeout in ' + maybeAbout + TimeoutManager.secondsToString(roundedSecondsRemaining);
     } else {
       this._timeoutText = 'Idle timeout is disabled';
     }
 
     this._runTimer();
-  }
-
-  // Rounds seconds to an approximate time, based on magnitude.
-  // We use rounding instead of truncating so that the user will generally see
-  // the number they specified, such as "3h", rather than something like "2h 50m".
-  private _roundToApproximateTime(seconds: number): number {
-    if (seconds <= 2 * TimeoutPanel._secondsPerMinute) {
-      return seconds;  // No rounding when less than 2 minutes.
-    }
-    const minutes = Math.round(seconds / TimeoutPanel._secondsPerMinute);
-    if (minutes <= 20) {
-      // Round to nearest minute when under 20.5 minutes.
-      return minutes * TimeoutPanel._secondsPerMinute;
-    }
-    if (minutes <= 60) {
-      // Round to nearest 5 minutes when under 1.5 hour.
-      return Math.round(minutes / 5) * 5 * TimeoutPanel._secondsPerMinute;
-    }
-    const hours = Math.round(minutes / TimeoutPanel._secondsPerMinute);
-    if (hours <= 2) {
-      // Round to nearest 10 minutes when under 2.5 hours.
-      return Math.round(minutes / 10) * 10 * TimeoutPanel._secondsPerMinute;
-    }
-    if (hours <= 5) {
-      // Round to nearest half hour when under 5.5 hours.
-      return Math.round(minutes / 30) * 30 * TimeoutPanel._secondsPerMinute;
-    }
-    const days = Math.round(hours / TimeoutPanel._hoursPerDay);
-    if (days <= 3) {
-      // Round to nearest hour when under 3.5 days.
-      return hours * TimeoutPanel._secondsPerHour;
-    }
-    // Round to nearest day when 3.5 days or more.
-    return days * TimeoutPanel._secondsPerDay;
-  }
-
-  // Converts a number of seconds into a string that include m, h, and d units.
-  private _secondsToString(seconds: number): string {
-    let s = '';         // build a string
-    let t = seconds;    // number of seconds left to convert
-    let sep = '';       // separator, gets set to space once s is not null
-    if (t > TimeoutPanel._secondsPerDay) {
-      const days = Math.floor(t / TimeoutPanel._secondsPerDay);
-      t = t % TimeoutPanel._secondsPerDay;
-      s = s + sep + days + 'd';
-      sep = ' ';
-    }
-    if (t > TimeoutPanel._secondsPerHour) {
-      const hours = Math.floor(t / TimeoutPanel._secondsPerHour);
-      t = t % TimeoutPanel._secondsPerHour;
-      s = s + sep + hours + 'h';
-      sep = ' ';
-    }
-    if (t > TimeoutPanel._secondsPerMinute) {
-      const minutes = Math.floor(t / TimeoutPanel._secondsPerMinute);
-      t = t % TimeoutPanel._secondsPerMinute;
-      s = s + sep + minutes + 'm';
-      sep = ' ';
-    }
-    if (t > 0) {
-      s = s + sep + t + 's';
-    }
-    return s;
   }
 
   /**
