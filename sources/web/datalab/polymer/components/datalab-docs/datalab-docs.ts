b80b025b03756c83d2284371d26fe68494577afb
Yasser Elsayed <yebrahim@users.noreply.github.com>
2017-09-15 11:43:16 -0700

    Add a Docs page (#1674)
    The Docs page appears in the sidebar and loads the docs from Github.
    
    * added docs element with default drive file browser
    
    * use the github path/fileid for docs

---
+++ sources/web/datalab/polymer/components/datalab-docs/datalab-docs.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/// <reference path="../input-dialog/input-dialog.ts" />
+/// <reference path="../item-list/item-list.ts" />
+
+/**
+ * Docs element for Datalab.
+ */
+class DatalabDocsElement extends Polymer.Element implements DatalabPageElement {
+
+  static get is() { return 'datalab-docs'; }
+
+  static get properties() {
+    return {
+      fileId: {
+        notify: true,
+        type: String,
+      },
+    };
+  }
+
+  /**
+   * Pass through requests to our file-browser element.
+   */
+  focusHandler() {
+    this.$.fileBrowser.focusHandler();
+  }
+  blurHandler() {
+    this.$.fileBrowser.blurHandler();
+  }
+  resizeHandler() {
+    this.$.fileBrowser.resizeHandler();
+  }
+}
+
+customElements.define(DatalabDocsElement.is, DatalabDocsElement);
