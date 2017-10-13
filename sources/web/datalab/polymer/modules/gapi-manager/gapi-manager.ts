8e91199f06003f1146d9ab75866d60fe858d9cbd
Yasser Elsayed <yebrahim@users.noreply.github.com>
2017-10-13 13:46:15 -0700

    Use host root for redirect URL (#1746)
    * Use host root for redirect URL
    
    This is needed because the URL requiring the redirect can be any arbitrary path, now that file paths are part of the URL.
    
    * comment

--- sources/web/datalab/polymer/modules/gapi-manager/gapi-manager.ts
+++ sources/web/datalab/polymer/modules/gapi-manager/gapi-manager.ts
@@ -407,9 +407,12 @@ class GapiManager {
   * OAuth 2.0 client ID and scopes (space delimited string) to request access.
   */
   private static _initClient(): Promise<void> {
+    // TODO: Add state parameter to redirect the user back to the current URL
+    // after the OAuth flow finishes.
     return gapi.auth2.init({
       client_id: GapiManager._clientId,
       fetch_basic_profile: true,
+      redirect_uri: Utils.getHostRoot(),
       scope: initialScopeString,
       ux_mode: 'redirect',
     })
